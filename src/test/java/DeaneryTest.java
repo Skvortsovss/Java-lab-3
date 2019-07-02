e1f970d67b802d13c6d548d40413e551153259c0
Skvortsovss <skvortsovss@mail.ru>
2019-07-02 13:21:03 +0300

    test

--- src/test/java/DeaneryTest.java
+++ src/test/java/DeaneryTest.java
@@ -8,14 +8,14 @@ public class DeaneryTest {
     public void studentTransfer() {
         Deanery deanery = new Deanery();
         Student st1 = new Student(1,"Сидоров Семен");
-        Student st2 = new Student(2,"рогов Максим");
+        Student st2 = new Student(2,"Рогов Максим");
         Group gr1 = new Group("Economists");
-        Group gr2 = new Group("lawyers");
+        Group gr2 = new Group("Engineers");
         deanery.getGroups().add(gr1);
         deanery.getGroups().add(gr2);
         deanery.getGroups().get(deanery.getGroups().indexOf(gr1)).addStudent(st1);
         deanery.getGroups().get(deanery.getGroups().indexOf(gr2)).addStudent(st2);
-        deanery.studentTransfer(1,"lawyers");
+        deanery.studentTransfer(1,"Economists");
         String actual = deanery.getGroups().get(deanery.getGroups().indexOf(gr2)).getStaff().get(deanery.getGroups().get(deanery.getGroups().indexOf(gr2)).getStaff().indexOf(st1)).getFIO();
         String expected = "Сидоров Семен";
         assertEquals(actual,expected);
